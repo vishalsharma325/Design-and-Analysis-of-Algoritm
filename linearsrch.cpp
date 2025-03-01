88b3b87c3cce8a8f8f8c11ba67f25f77ac959ce8
vishalsharma325 <156929772+vishalsharma325@users.noreply.github.com>
2025-03-01 13:05:24 +0530

    Update linearsrch.cpp

--- linearsrch.cpp
+++ linearsrch.cpp
@@ -16,7 +16,7 @@ int main() {
     cin>>arr[i];
    }
     int size = 8;
-    cout<<"enter the key to be searched :";
+    cout<<"enter the element to be searched :";
     int key;
     cin>>key;
     int result = linearSearch(arr, size, key);
